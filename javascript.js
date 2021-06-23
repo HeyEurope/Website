3e48b5172ea7523f79e557afd2bfe6a72dee0fa5
muliPuli <milan.paczai20@imperial.ac.uk>
2021-06-23 17:02:14 +0200

    added logos+updates to website text and design

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const landingPage = document.querySelector(".landingPage");
 
+const sections = document.getElementsByClassName("section");
+
 const header = document.querySelector("header");
 
 
@@ -7,9 +9,19 @@ const header = document.querySelector("header");
 let setHeight = () => {
     if (window.innerWidth > 500) {
         landingPage.style.minHeight = window.innerHeight + "px";
+        for (let i = 0; i < sections.length; i++) {
+            sections[i].style.minHeight = window.innerHeight + "px";
+            console.log("i run");
+        }
+        console.log(sections);
     } else {
         landingPage.style.minHeight = window.innerHeight + "px";
         landingPage.style.height = "auto";
+        for (let i = 0; i < sections.length; i++) {
+            sections[i].style.minHeight = window.innerHeight + "px";
+            sections[i].style.height = "auto";
+            console.log("i run");
+        }
     }
 
 }
