047d40f0609d0be0d7e60b9f8ce8b4147fde2070
muliPuli <milan.paczai20@imperial.ac.uk>
2021-10-28 09:56:25 +0100

    added 5 members to team

---
+++ components/memberProfile.js
@@ -0,0 +1,48 @@
+// 'use strict';
+
+// import React from 'react';
+
+// import { FaBeer } from 'react-icons/fa';
+
+
+
+
+
+// class MemberProfile extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { liked: false };
+//     }
+
+//     render() {
+
+
+//         return (
+            // <div style={{ display: 'flex', height: 400 + 'px', justifyContent: 'center', flexWrap: "wrap", alignItems: 'center', flexDirection: 'column' }}>
+            //     <img src={"collecting.svg"}></img>
+            //     <h1>
+            //         Milan
+            //     </h1>
+            //     <p>Location: London</p>
+            //     <p>
+            //         Study:    Electrical Engineering
+            //     </p>
+            // </div>
+//         );
+//     }
+// }
+
+
+// ReactDOM.render(<MemberProfile />, document.getElementById("member-profiles"));
+
+
+// Find all DOM containers, and render Like buttons into them.
+// document.querySelectorAll('.member-profiles')
+//     .forEach(domContainer => {
+//         // Read the comment ID from a data-* attribute.
+
+//         ReactDOM.render(
+//             <div>MemberProfile</div>,
+//             domContainer
+//         );
+//     });
